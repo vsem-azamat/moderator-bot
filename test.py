87747c0c7817bc97c7fc57767fe4e1cf572be1d2
windows <almazbek1608@gmail.com>
2022-02-13 12:56:18 +0100

    3

--- test.py
+++ test.py
@@ -1,9 +1,40 @@
+import math
+
+def gen_inline_button(tuple_text: list, tuple_callback: list,
+                      row_wight: int = 1, ):
+    len_list = len(tuple_text)
+    row_quantity = math.ceil(len_list / row_wight)
+
+    list_dict = []
+    for i in range(len_list):
+        list_dict.append({"text": f"{tuple_text[i]}", "callback_data": f"{tuple_callback[i]}"})
+
+    finale_list = []
+    for j in range(row_quantity):
+        j = j * row_wight
+        finale_list.append(list_dict[j:j + row_wight])
+
+    return {"row_wight": row_wight, "inline_keyboard": finale_list}
 
 
+def gen_default_button(tuple_text: list, row_wight: int = 1):
+    len_list = len(tuple_text)
+    row_quantity = math.ceil(len_list / row_wight)
 
-f1 = ('first', 'second')
-f2 = (1, 2) 
+    list_dict = []
+    for text in tuple_text:
+        list_dict.append({"text": f"{text}"})
 
-test = zip(f1, f2)
+    finale_list = []
+    for j in range(row_quantity):
+        j = j * row_wight
+        finale_list.append(list_dict[j:j + row_wight])
 
-print(test[1])
+    return {"keyboard": finale_list, "resize_keyboard": True}
+
+
+class GenButtons():
+
+    def default_b(l_text, width):
+        
+        return {"keyboard:": finale_list, "resize_keyboard": True}
