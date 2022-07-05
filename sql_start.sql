d3190f2d095a48fe6e11ec42c6f305af09e5e3d9
almazbek <almazbek1608@gmail.com>
2022-07-05 16:31:02 +0200

    fix

---
+++ sql_start.sql
@@ -0,0 +1,17 @@
+INSERT INTO chatlinks (text, link)
+VALUES
+    ('ČVUT',    't.me/cvut_chat'),
+    ('VŠE',     't.me/vse_chat'),
+    ('Karlov',  't.me/karlov_chat'),
+    ('ČZU',     't.me/czu_chat'),
+    ('VŠCHT',   't.me/vscht_chat'),
+    ('VUT',     't.me/vut_chat'),
+    ('Masaryk', 't.me/masaryk_chat'),
+    ('GoStudy', 't.me/Gostudy_20'),
+    ('Центральный чат', 't.me/czechopen')
+;
+
+INSERT INTO commands (id, command, state)
+VALUES
+    (1, 'gay', 1)
+;
