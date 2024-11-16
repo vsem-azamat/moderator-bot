9b7e2b78880b173a9fe303020aed84480aadaa6c
vsem-azamat <almazbek1608@gmail.com>
2024-11-17 00:12:29 +0100

    feat: Modify new chat links insert queries

---
+++ scripts/insert_chat_links.sql
@@ -0,0 +1,9 @@
+INSERT INTO chat_links (text, link) VALUES ('ČVUT', 't.me/cvut_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('VŠE', 't.me/vse_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('Karlov', 't.me/karlov_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('ČZU', 't.me/czu_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('VŠCHT', 't.me/vscht_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('VUT', 't.me/vut_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('Masaryk', 't.me/masaryk_chat') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('GoStudy', 't.me/Gostudy_20') ON CONFLICT DO NOTHING/;
+INSERT INTO chat_links (text, link) VALUES ('Образовательный чат', 't.me/czechopen') ON CONFLICT DO NOTHING/;
