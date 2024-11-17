cd1c14aafec3d196657654d92675132eb5905e16
vsem-azamat <almazbek1608@gmail.com>
2024-11-17 15:32:56 +0100

    fix: update message model_dump to exclude only None values in save_message function

--- bot/services/history.py
+++ bot/services/history.py
@@ -7,7 +7,7 @@ async def save_message(db, message: types.Message) -> None:
     chat_id = message.chat.id
     user_id = message.from_user.id
     message_text = message.text
-    message_info = message.model_dump(exclude_none=True, exclude_unset=True)
+    message_info = message.model_dump(exclude_none=True)
 
     message_repo = get_message_repository(db)
     await message_repo.add_message(chat_id, user_id, message_text, message_info)
