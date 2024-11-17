cd85d0c67ed4d5d636a9c3971f426f0515f439d8
vsem-azamat <almazbek1608@gmail.com>
2024-11-17 15:15:17 +0100

    fix: update middleware registration for HistoryMiddleware in bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -38,7 +38,7 @@ async def get_bot_and_dp() -> tuple[Bot, Dispatcher]:
 async def main() -> None:
     bot, dp = await get_bot_and_dp()
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
-    dp.message.middleware(HistoryMiddleware())
+    dp.update.middleware(HistoryMiddleware())
     dp.message.middleware(BlacklistMiddleware(bot))
     dp.message.middleware(ManagedChatsMiddleware(bot))
     dp.callback_query.middleware(CallbackAnswerMiddleware())
