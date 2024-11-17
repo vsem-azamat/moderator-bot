c1de15d54686a9d6e906d667cb9fd336020486a6
vsem-azamat <almazbek1608@gmail.com>
2024-11-17 16:05:55 +0100

    feat: include message_id in save_message function for improved message tracking

--- bot/services/history.py
+++ bot/services/history.py
@@ -6,11 +6,18 @@ from database.repositories import get_message_repository, get_user_repository
 async def save_message(db, message: types.Message) -> None:
     chat_id = message.chat.id
     user_id = message.from_user.id
+    message_id = message.message_id
     message_text = message.text
     message_info = message.model_dump(exclude_none=True)
 
     message_repo = get_message_repository(db)
-    await message_repo.add_message(chat_id, user_id, message_text, message_info)
+    await message_repo.add_message(
+        chat_id, 
+        user_id,
+        message_id, 
+        message_text, 
+        message_info
+    )
 
 
 async def save_user(db, user: types.User) -> None:
