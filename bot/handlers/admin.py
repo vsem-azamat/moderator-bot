835929b66775927a2d205f063cca7612c763291c
vsem-azamat <almazbek1608@gmail.com>
2024-12-06 10:02:23 +0100

    feat: add admin existence checks in admin commands

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -10,13 +10,21 @@ router = Router()
 
 @router.message(Command("admin", prefix="!/"))
 async def new_admin(message: types.Message, admin_repo: AdminRepository):
-    await admin_repo.insert_admin(message.reply_to_message.from_user.id)
-    await message.answer(f"Админ {await other.get_user_mention(message.reply_to_message.from_user)} добавлен.")
+    target_user = message.reply_to_message.from_user
+    if not await admin_repo.is_admin(target_user.id):
+        await admin_repo.insert_admin(target_user.id)
+        await message.answer(f"Админ {await other.get_user_mention(target_user)} добавлен.")
+    else:
+        await message.answer(f"Админ {await other.get_user_mention(target_user)} уже есть в базе.")
     await message.delete()
 
 
 @router.message(Command("unadmin", prefix="!/"))
 async def delete_admin(message: types.Message, admin_repo: AdminRepository):
-    await admin_repo.delete_admin(message.reply_to_message.from_user.id)
-    await message.answer(f"Админ {await other.get_user_mention(message.reply_to_message.from_user)} удален.")
+    target_user = message.reply_to_message.from_user
+    if await admin_repo.is_admin(target_user.id):
+        await admin_repo.delete_admin(target_user.id)
+        await message.answer(f"Админ {await other.get_user_mention(target_user)} удален.")
+    else:
+        await message.answer(f"Админ {await other.get_user_mention(target_user)} не является админом.")
     await message.delete()
