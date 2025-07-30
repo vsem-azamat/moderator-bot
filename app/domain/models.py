3af613b02a87a0c61373f98efe2c5bf32b0d7710
Azamat <60985938+vsem-azamat@users.noreply.github.com>
2025-07-30 09:27:07 +0200

    Fix base import and complete compose file

--- app/domain/models.py
+++ app/domain/models.py
@@ -2,7 +2,7 @@ import datetime
 from sqlalchemy import Integer, String, Boolean, BigInteger, DateTime, JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .base import Base
+from app.infrastructure.db.base import Base
 
 
 class Admin(Base):
